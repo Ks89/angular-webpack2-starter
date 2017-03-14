59c07062b42514f0ed62efb24e38a0839873cd63
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-03-14 22:24:39 +0100

    updated to angular-modal-gallery rc.1

--- src/app/features/dashboard.component.ts
+++ src/app/features/dashboard.component.ts
@@ -16,7 +16,7 @@ import { Image, Action, ImageModalEvent, Description } from 'angular-modal-galle
 @Component({
   selector: 'my-dashboard',
   templateUrl: './dashboard.component.html',
-  styles: [`#my-logout-button { background: #F44336 }`]
+  styleUrls: ['./dashboard.css']
 })
 
 export class DashboardComponent implements OnDestroy, OnInit {
@@ -28,53 +28,53 @@ export class DashboardComponent implements OnDestroy, OnInit {
 
   // ---------------------------------------------------------------------------------
   // angular-modal-gallery
-  public openModalWindow: boolean = false;
-  public imagePointer: number = 0;
+  openModalWindow: boolean = false;
+  imagePointer: number = 0;
 
-  public openModalWindowObservable: boolean = false;
-  public imagePointerObservable: number = 0;
+  openModalWindowObservable: boolean = false;
+  imagePointerObservable: number = 0;
 
-  public imagesArray: Image[] = [
+  imagesArray: Array<Image> = [
     new Image(
-      '../../assets/images/gallery/img1.jpg',
+      '../../../../assets/images/gallery/img1.jpg',
       null, // no thumb
       null, // no description
       'http://www.google.com'
     ),
     new Image(
-      '../../assets/images/gallery/img2.png', // example with a PNG image
+      '../../../../assets/images/gallery/img2.png', // example with a PNG image
       null, // no thumb
       'Description 2',
       null // url
     ),
     new Image(
-      '../../assets/images/gallery/img3.jpg',
-      '../../assets/images/gallery/thumbs/img3.png', // example with a PNG thumb image
+      '../../../../assets/images/gallery/img3.jpg',
+      '../../../../assets/images/gallery/thumbs/img3.png', // example with a PNG thumb image
       'Description 3',
       'http://www.google.com'
     ),
     new Image(
-      '../../assets/images/gallery/img4.jpg',
+      '../../../../assets/images/gallery/img4.jpg',
       null, // no thumb
       'Description 4',
       'http://www.google.com'
     ),
     new Image(
-      '../../assets/images/gallery/img5.jpg',
-      '../../assets/images/gallery/thumbs/img5.jpg',
+      '../../../../assets/images/gallery/img5.jpg',
+      '../../../../assets/images/gallery/thumbs/img5.jpg',
       null, // no description
       null // url
     )
   ];
 
   // observable of an array of images with a delay to simulate a network request
-  public images: Observable<Image[]> = Observable.of(this.imagesArray).delay(300);
+  images: Observable<Array<Image>> = Observable.of(this.imagesArray).delay(300);
 
   // array with a single image inside (the first one)
-  public singleImage: Observable<Image[]> = Observable.of([
+  singleImage: Observable<Array<Image>> = Observable.of([
     new Image(
-      '../../assets/images/gallery/img1.jpg',
-      '../../assets/images/gallery/thumbs/img1.jpg',
+      '../../../../assets/images/gallery/img1.jpg',
+      '../../../../assets/images/gallery/thumbs/img1.jpg',
       'Description 1',
       'http://www.google.com'
     )]
@@ -84,15 +84,15 @@ export class DashboardComponent implements OnDestroy, OnInit {
   // in an asynchronous way subscribing to an Observable with a delay.
   // This is not a real use-case, but it's a way to simulate a scenario where
   // you have to subscribe to an Observable to get data and to set public vars
-  public imagesArraySubscribed: Image[];
+  imagesArraySubscribed: Array<Image>;
 
-  public customDescription: Description = {
+  customDescription: Description = {
     imageText: 'Look this image ',
     numberSeparator: ' of ',
     beforeTextDescription: ' => '
   };
 
-  public customFullDescription: Description = {
+  customFullDescription: Description = {
     // you should build this value programmaticaly with the result of (show)="..()" event
     customFullDescription: 'Custom description of the current visible image',
     // if customFullDescription !== undefined, all other fields will be ignored
@@ -126,15 +126,15 @@ export class DashboardComponent implements OnDestroy, OnInit {
 
   // ----------------------
   // angular-modal-gallery
-  public openImageModal(image: Image) {
+  openImageModal(image: Image) {
     this.imagePointer = this.imagesArray.indexOf(image);
     this.openModalWindow = true;
   }
 
-  public openImageModalObservable(image: Image) {
+  openImageModalObservable(image: Image) {
     this.subscription = this.images.subscribe((val: Image[]) => {
-      this.imagePointer = val.indexOf(image);
-      this.openModalWindow = true;
+      this.imagePointerObservable = val.indexOf(image);
+      this.openModalWindowObservable = true;
     });
   }
 
@@ -165,9 +165,10 @@ export class DashboardComponent implements OnDestroy, OnInit {
     console.log('onClose action: ' + Action[event.action]);
     console.log('onClose result:' + event.result);
     this.openModalWindow = false;
+    this.openModalWindowObservable = false;
   }
 
-  public addRandomImage() {
+  addRandomImage() {
     this.imagesArray.push(this.imagesArray[Math.floor(Math.random() * this.imagesArray.length)]);
   }
   // ----------------------
